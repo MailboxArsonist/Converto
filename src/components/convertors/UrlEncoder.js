5bb85708c38b006ad45030ec3e3cfde98c9736e0
Matthew Dodd <matthew.dodd93@gmail.com>
2020-04-13 17:04:09 +0200

    up url encoder success button

--- src/components/convertors/UrlEncoder.js
+++ src/components/convertors/UrlEncoder.js
@@ -33,11 +33,14 @@ const UrlEncoder = () => {
 
       <p className="text-center mb-4">Enter your url to encode in the box below.</p>
 
-      <textarea value={ urlInput } onChange={ e => handleInput(e.target.value) } className="mx-auto border shadow rounded block w-5/6 md:w-1/2 mb-10 p-3" placeholder="URL goes here" name="url-input" id="url-input" cols="30" rows="8" />
-      <textarea ref={textAreaRef} value={ encodedUrl } className="mx-auto border shadow rounded block w-5/6 md:w-1/2 p-3 mb-4" name="url-output" id="url-output" cols="30" rows="8" readOnly />
+      <textarea value={ urlInput } onChange={ e => handleInput(e.target.value) } className="mx-auto border shadow rounded block w-5/6 md:w-1/2 mb-10 p-3" placeholder="URL goes here..." name="url-input" id="url-input" cols="30" rows="8" />
+      <textarea ref={textAreaRef} value={ encodedUrl } className="mx-auto border shadow rounded block w-5/6 md:w-1/2 p-3 mb-4" placeholder="Et voila, now it's encoded!" name="url-output" id="url-output" cols="30" rows="8" readOnly />
       
-      <button onClick={handleCopy} type="button" className="w-5/6 md:w-1/2 text-center border rounded block mx-auto text-white px-2 py-2 bg-purple-700 hover:bg-purple-800">
-        { urlCopied ? "URL copied" : "Copy Encoded URL" }
+      <button onClick={handleCopy} type="button" className="w-5/6 md:w-1/2 text-center border rounded block mx-auto text-white px-2 py-3 bg-purple-700 hover:bg-purple-800">
+        { 
+          urlCopied 
+            ? <span className="flex items-center justify-center fill-current"><svg className="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 26" width="20" height="20"><path d="M17.62 10H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H8.5c-1.2 0-2.3-.72-2.74-1.79l-3.5-7-.03-.06A3 3 0 0 1 5 9h5V4c0-1.1.9-2 2-2h1.62l4 8zM16 11.24L12.38 4H12v7H5a1 1 0 0 0-.93 1.36l3.5 7.02a1 1 0 0 0 .93.62H16v-8.76zm2 .76v8h2v-8h-2z"/></svg>URL copied</span>
+            : "Copy Encoded URL" }
       </button>
     </>
   );
