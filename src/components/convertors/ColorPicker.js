3beba5921c7ef91577622c4fe03bf87165b0aecd
Matthew Dodd <matthew.dodd93@gmail.com>
2020-04-15 22:08:20 +0200

    Refactor eyedropper with new input

--- src/components/convertors/ColorPicker.js
+++ src/components/convertors/ColorPicker.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Input from "../Input"
 
 const ColorPicker = () => {
 
+  const [color, changeColor] = useState("#fefefe");
   const [img, setImg] = useState();
 
   function uploadImage(e){
@@ -19,7 +21,7 @@ const ColorPicker = () => {
   
   return (
     <>
-      <h1 className="text-3xl text-center mb-10">Color Picker</h1>
+      <h1 className="text-3xl text-center mb-10">Image Eyedropper</h1>
 
       <p className="text-center mb-6">Upload an image and then hover on the image to preview a color. Click to select the color.</p>
 
@@ -27,12 +29,17 @@ const ColorPicker = () => {
 
         <img src={img} className="max-w-md w-full mb-10" />
 
+        <div className="flex items-center mb-6">
+          <div style={{ backgroundColor: color }} className="w-16 h-16 block rounded-lg border-4 border-black"></div>
+          <Input className="border rounded shadow p-2 ml-3" value={color} readonly={true} />
+        </div>
+
+
         <label className="block w-5/6 md:w-1/2 text-center border rounded block mx-auto text-white px-2 py-3 bg-purple-700 hover:bg-purple-800 cursor-pointer" htmlFor="image-upload">
-          Upload an image <input className="hidden" type="file" id="image-upload" onChange={ e => uploadImage(e) } />
+          Upload an image <Input className="hidden" type="file" id="image-upload" handleInput={ e => uploadImage(e) } />
         </label>
 
       </div>
-
     </>
   );
 };
