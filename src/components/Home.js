c92290446e9fd82b8078c9a9ec5e6da94ae6a1e4
Matthew Dodd <matthew.dodd93@gmail.com>
2020-04-25 19:56:22 +0200

    home

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  const thing = "hello";
+  console.log(thing);
+  return (
+    <>
+      <h1 className="text-3xl text-center mb-10">Converto</h1>
+
+      <p className="text-center mb-4">Welcome to converto.</p>
+
+    </>
+  );
+};
+export default Home;
